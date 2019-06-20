50bfb47c1ff351c8afaa7aa9e59eb4dacd5d1f7a
Jamie Hardt <jamiehardt@me.com>
2019-06-19 19:08:43 -0700

    Timecode/string lossless convertible

--- Sources/Timco/TimecodeStringRep.swift
+++ Sources/Timco/TimecodeStringRep.swift
@@ -7,12 +7,30 @@
 
 import Foundation
 
-extension TimecodeRep : CustomStringConvertible {
-    
+extension TimecodeRep : LosslessStringConvertible {
     var description : String {
         return String(format: "%02i:%02i:%02i:%02i",
                           self.hours, self.minutes,
                           self.seconds,  self.frames)
     }
-    
+
+    init?(_ description: String) {
+        let scanner = Scanner(string: description)
+        
+        var hh : Int = 0
+        var mm : Int = 0
+        var ss : Int = 0
+        var ff : Int = 0
+        
+        guard scanner.scanInt(&hh) else { return nil }
+        guard scanner.scanString(":", into: nil) else { return nil }
+        guard scanner.scanInt(&mm) else { return nil }
+        guard scanner.scanString(":", into: nil) else { return nil }
+        guard scanner.scanInt(&ss) else { return nil }
+        guard scanner.scanString(":", into: nil) ||
+            scanner.scanString(":", into: nil) else { return nil }
+        guard scanner.scanInt(&ff) else { return nil }
+        
+        self.init(hours: hh, minutes: mm, seconds: ss, frames : ff)
+    }
 }
--- Tests/TimcoTests/TimcoTests.swift
+++ Tests/TimcoTests/TimcoTests.swift
@@ -45,6 +45,18 @@ final class TimcoTests: XCTestCase {
         let r1 = TimecodeRep(hours: 1, minutes: 0, seconds: 0, frames: 0)
         XCTAssertTrue(r1.valid(for: .Count30Drop))
     }
+    
+    func testFromString() {
+        guard let r1 = TimecodeRep("01:14:19:03") else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertTrue(r1.hours == 1)
+        XCTAssertTrue(r1.minutes == 14)
+        XCTAssertTrue(r1.seconds == 19)
+        XCTAssertTrue(r1.frames == 3)
+    }
 
     static var allTests = [
         ("testDropType", testDropType),
@@ -52,6 +64,7 @@ final class TimcoTests: XCTestCase {
         ("testTimecodeRepDrop", testTimecodeRepDrop),
         ("testStringRep",testStringRep),
         ("testStringRepToFrameCount",testStringRepToFrameCount),
-        ("testValidation",testValidation)
+        ("testValidation",testValidation),
+        ("testFromString",testFromString)
     ]
 }
