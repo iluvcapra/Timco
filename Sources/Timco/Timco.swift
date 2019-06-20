eb8a62d7182876b49d83ef015f7c10fe629cbe5f
Jamie Hardt <jamiehardt@me.com>
2019-06-19 17:59:58 -0700

    First commit, time code reps and drop frame

--- Sources/Timco/Timco.swift
+++ Sources/Timco/Timco.swift
@@ -1,3 +1,72 @@
-struct Timco {
-    var text = "Hello, World!"
+typealias FrameCount = Int
+
+enum TimecodeFrameCountMode {
+    case Count24
+    case Count25
+    case Count30
+    case Count30Drop
+    
+    var integralFPS : Int {
+        switch self {
+        case .Count24: return 24
+        case .Count25: return 25
+        case .Count30, .Count30Drop:
+            return 30
+        }
+    }
+    
+    var dropFrame : Bool {
+        switch self {
+        case .Count30Drop:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+struct TimecodeRep  {
+    
+    let hours : Int
+    let minutes : Int
+    let seconds : Int
+    let frames : Int
+    let dropFrame : Bool
+    
+    static func dropFrameCount(_ fcm: TimecodeFrameCountMode, _ absoluteFrameCount: FrameCount) -> FrameCount {
+        precondition(fcm.integralFPS == 30)
+        let framesPerMinute = 30 * 60
+        
+        let (mins, _) = absoluteFrameCount
+            .quotientAndRemainder(dividingBy: framesPerMinute)
+        
+        let (tenMins, _) = mins.quotientAndRemainder(dividingBy: 10)
+        
+        return (mins * 2) - (tenMins * 2)
+    }
+    
+    static func modulateFrameCount(_ correctedFrameCount: FrameCount,
+                                   _ fcm: TimecodeFrameCountMode) -> (Int, Int, Int, Int) {
+        let secondsPerMinute = 60
+        let minutesPerHour = 60
+        let (secs, ff) = (correctedFrameCount as Int).quotientAndRemainder(dividingBy: fcm.integralFPS)
+        let (mins, ss) = secs.quotientAndRemainder(dividingBy: secondsPerMinute)
+        let (hours, mm) = mins.quotientAndRemainder(dividingBy: minutesPerHour)
+        let hh = hours % 24
+        
+        return (hh, mm, ss, ff)
+    }
+    
+    static func with(frameCount: FrameCount, fcm: TimecodeFrameCountMode) -> TimecodeRep {
+        
+        let absoluteFrameCount = abs(frameCount)
+        let dropCorrection : Int = fcm.dropFrame ? dropFrameCount(fcm, absoluteFrameCount) : 0
+        let correctedFrameCount : FrameCount = absoluteFrameCount + dropCorrection
+        let (hh, mm, ss, ff) : (Int,Int,Int,Int) = modulateFrameCount(correctedFrameCount, fcm)
+        
+        return TimecodeRep(hours: hh, minutes: mm, seconds: ss, frames: ff,
+                           dropFrame: fcm.dropFrame)
+    }
+    
+    
 }
--- Tests/TimcoTests/TimcoTests.swift
+++ Tests/TimcoTests/TimcoTests.swift
@@ -2,14 +2,34 @@ import XCTest
 @testable import Timco
 
 final class TimcoTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Timco().text, "Hello, World!")
+
+    func testDropType() {
+        XCTAssertFalse(TimecodeFrameCountMode.Count24.dropFrame)
+        XCTAssertTrue(TimecodeFrameCountMode.Count30Drop.dropFrame)
+    }
+    
+    func testTimecodeRepNonDrop() {
+        let frameCount = 40
+        let r = TimecodeRep.with(frameCount: frameCount, fcm: .Count24)
+        XCTAssertEqual(r.hours, 0)
+        XCTAssertEqual(r.minutes, 0)
+        XCTAssertEqual(r.seconds, 1)
+        XCTAssertEqual(r.frames, 16)
+        XCTAssertFalse(r.dropFrame)
+    }
+    
+    func testTimecodeRepDrop() {
+        let frameCount = 1830 // 30 * 61
+        let r = TimecodeRep.with(frameCount: frameCount, fcm: .Count30Drop)
+        XCTAssertEqual(r.hours, 0)
+        XCTAssertEqual(r.minutes, 1)
+        XCTAssertEqual(r.seconds, 1)
+        XCTAssertEqual(r.frames, 2)
+        XCTAssertTrue(r.dropFrame)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testDropType", testDropType),
+        ("testTimecodeRepNonDrop", testTimecodeRepNonDrop)
     ]
 }
